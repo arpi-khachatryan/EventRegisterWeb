0694e47bf34d8c08925f8794c41d292cb6afad9b
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-02 21:42:26 +0400

    DeleteUserServlet

---
+++ src/main/java/servlet/DeleteUserServlet.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import manager.EventManager;
+import manager.UserManager;
+import model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(urlPatterns = "/users/delete")
+public class DeleteUserServlet extends HttpServlet {
+    UserManager userManager = new UserManager();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<User> usersList = userManager.getAll();
+        req.setAttribute("users", usersList);
+        req.getRequestDispatcher("/WEB-INF/deleteUser.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        userManager.deleteUser(id);
+        resp.sendRedirect("/users");
+    }
+}
