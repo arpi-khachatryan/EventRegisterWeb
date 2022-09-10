f87f20a38aeb0dd04d2553b033ffdfd8a37a8334
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-10 17:41:20 +0400

    UserManager

--- src/main/java/manager/UserManager.java
+++ src/main/java/manager/UserManager.java
@@ -3,6 +3,7 @@ package manager;
 import db.DBConnectionProvider;
 import model.Event;
 import model.User;
+import model.UserRole;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -13,13 +14,16 @@ public class UserManager {
     private EventManager eventManager = new EventManager();
 
     public void add(User user) {
-        String sql = "insert into user(name,surname,email,event_id) values(?,?,?,?)";
+        String sql = "insert into user(name,surname,email,profile_pic,password,user_role) values(?,?,?,?,?,?)";
         try {
             PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, user.getName());
             ps.setString(2, user.getSurname());
             ps.setString(3, user.getEmail());
-            ps.setInt(4, user.getEvent().getId());
+//            ps.setInt(4, user.getEvent().getId());
+            ps.setString(4, user.getProfilePic());
+            ps.setString(5, user.getPassword());
+            ps.setString(6, user.getUserRole().name());
             ps.executeUpdate();
             ResultSet resultSet = ps.getGeneratedKeys();
             if (resultSet.next()) {
@@ -60,6 +64,37 @@ public class UserManager {
         return null;
     }
 
+    public User getUserByEmailAndPassword(String email, String password) {
+        String sql = "select * from user where email=? and password=?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, email);
+            ps.setString(2, password);
+            ResultSet resultSet = ps.executeQuery();
+            if (resultSet.next()) {
+                return getUserFromResultSet(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public User getUserByEmail(String email) {
+        String sql = "select * from user where email=?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, email);
+            ResultSet resultSet = ps.executeQuery();
+            if (resultSet.next()) {
+                return getUserFromResultSet(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     private User getUserFromResultSet(ResultSet resultSet) throws SQLException {
         int id = resultSet.getInt("event_id");
         Event event = eventManager.getById(id);
@@ -69,6 +104,9 @@ public class UserManager {
                 .surname(resultSet.getString("surname"))
                 .email(resultSet.getString("email"))
                 .event(event)
+                .profilePic(resultSet.getString("profile_pic"))
+                .password(resultSet.getString("password"))
+                .userRole(UserRole.valueOf(resultSet.getString("user_role")))
                 .build();
     }
 
@@ -83,14 +121,17 @@ public class UserManager {
     }
 
     public void edit(User user) {
-        String sql = "update user set name=?,surname=?,email=?, event_id=? where id=?";
+        String sql = "update user set name=?,surname=?,email=?, event_id=?, profile_pic=?,password=?,user_role=? where id=?";
         try {
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.setString(1, user.getName());
             ps.setString(2, user.getSurname());
             ps.setString(3, user.getEmail());
             ps.setInt(4, user.getEvent().getId());
-            ps.setInt(5, user.getId());
+            ps.setString(5, user.getProfilePic());
+            ps.setString(6, user.getPassword());
+            ps.setString(7, user.getUserRole().name());
+            ps.setInt(8, user.getId());
             ps.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
