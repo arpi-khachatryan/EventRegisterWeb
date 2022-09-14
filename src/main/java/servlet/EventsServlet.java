ffb8479265f31e4e3f335a5b6ca83280a9737cef
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-14 23:00:51 +0400

    EventsServlet

--- src/main/java/servlet/EventsServlet.java
+++ src/main/java/servlet/EventsServlet.java
@@ -1,7 +1,9 @@
 package servlet;
 
 import manager.EventManager;
+import manager.UserManager;
 import model.Event;
+import model.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,10 +16,32 @@ import java.util.List;
 @WebServlet(urlPatterns = "/events")
 public class EventsServlet extends HttpServlet {
     private EventManager eventManager = new EventManager();
+    private UserManager userManager = new UserManager();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<Event> eventList = eventManager.getAll();
+        String keyword = req.getParameter("keyword");
+        double minPrice = Double.parseDouble(req.getParameter("minPrice") == null ? "0" : req.getParameter("minPrice"));
+        double maxPrice = Double.parseDouble(req.getParameter("maxPrice") == null ? "0" : req.getParameter("maxPrice"));
+
+        List<Event> eventList;
+        if (keyword != null && !keyword.equals("")) {
+            req.setAttribute("keyword", keyword);
+            eventList = eventManager.search(keyword);
+        } else if (minPrice > 0 || maxPrice > 0 && minPrice < maxPrice) {
+            req.setAttribute("keyword", "");
+            eventList = eventManager.filter(minPrice, maxPrice);
+        } else {
+            req.setAttribute("keyword", "");
+            eventList = eventManager.getAll();
+        }
+
+
+
+        for (Event event : eventList) {
+            List<User> users = userManager.getUsersByEventId(event.getId());
+            event.setUsers(users);
+        }
         req.setAttribute("events", eventList);
         req.getRequestDispatcher("/WEB-INF/events.jsp").forward(req, resp);
     }
