bf5dc2c0e9b7d62e51a58767e07ccf96b31651e9
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-14 22:58:20 +0400

    UserFilter

--- src/main/java/filter/UserFilter.java
+++ src/main/java/filter/UserFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = "/users")
+@WebFilter(urlPatterns = "/events")
 public class UserFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
