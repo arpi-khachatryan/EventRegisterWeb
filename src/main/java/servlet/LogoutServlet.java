bed5cf83906388b3f945fec5a205224499053125
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-14 22:59:12 +0400

    LogoutServlet

--- src/main/java/servlet/LogoutServlet.java
+++ src/main/java/servlet/LogoutServlet.java
@@ -13,6 +13,6 @@ public class LogoutServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getSession().invalidate();
-        resp.sendRedirect("/index.jsp");
+        resp.sendRedirect("/");
     }
 }
